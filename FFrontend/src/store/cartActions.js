b17bb498f7773a3238c728ba66d6094d89095b60
Hanzla Nouman <hanzlanouman514@gmail.com>
2024-05-01 22:04:02 +0500

    Working cart items increasing

--- FFrontend/src/store/cartActions.js
+++ FFrontend/src/store/cartActions.js
@@ -1,25 +1,11 @@
-// import { ADD_TO_CART } from "./actionTypes"; 
-// import axios from "axios";      
 
-// // export const addItemsToCart = (id, quantity,cartItems) => async (dispatch) => {
-// //   const {data} = await axios.get(`http://localhost:4000/api/v1/product/${id}`);
-// //   console.log("cartAction",cartItems)
-// //   dispatch({   
-// //     type: ADD_TO_CART,       
-// //     payload: {       
-// //       price: data.product.price,    
-// //       product: data.product._id,     
-// //       name: data.product.name,       
-// //       image: data.product.images[0].url, 
-// //       stock: data.product.stock,
-// //       quantity,
-// //     },
-// //   });
-// //   localStorage.setItem("cartItems", JSON.stringify([...cartItems]));
-// // };
-// export const addItemsToCart = (id, quantity, cartItems) => async (dispatch) => {
+
+import { ADD_TO_CART } from "./actionTypes";
+import axios from "axios";
+
+
+// export const addItemsToCart = (id, quantity,cartItems) => async (dispatch) => {
 //   const { data } = await axios.get(`http://localhost:4000/api/v1/product/${id}`);
-//   // console.log("cartAction", cartItems);
 //   dispatch({
 //     type: ADD_TO_CART,
 //     payload: {
@@ -32,8 +18,6 @@
 //     },
 //   });
 
-  
-//   // Update local storage after updating Redux state
 //   const updatedCartItems = [...cartItems, { 
 //     price: data.product.price,
 //     product: data.product._id,
@@ -42,26 +26,31 @@
 //     stock: data.product.stock,
 //     quantity,
 //   }];
+
+// if (localStorage.getItem("cartItems")) {
+//   console.log(updatedCartItems,"updated")
+//   let itemFromStorage = localStorage.getItem("cartItems");
+
+//   // Parse the item if it exists
   
+//     itemFromStorage = JSON.parse(itemFromStorage);
+//     // Update the item as needed
+//     itemFromStorage.quantity = quantity;
+  
+//     // Store the updated item back into localStorage
+//     localStorage.setItem("cartItems", JSON.stringify(itemFromStorage));
+
+// }else{
+//   console.log("not presence")
 //   localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
- 
-// };
-// store/cartActions.js
-
-import { ADD_TO_CART } from "./actionTypes";
-import axios from "axios";
-
-// export const loadCartItemsFromStorage = () => async (dispatch) => {
-//   const cartItemsFromStorage = localStorage.getItem("cartItems");
-//   if (cartItemsFromStorage) {
-//     dispatch({
-//       type: ADD_TO_CART,
-//       payload: JSON.parse(cartItemsFromStorage),
-//     });
-//   }
+// }
 // };
 
-export const addItemsToCart = (id, quantity,cartItems) => async (dispatch) => {
+
+
+
+
+export const addItemsToCart = (id, quantity, cartItems) => async (dispatch) => {
   const { data } = await axios.get(`http://localhost:4000/api/v1/product/${id}`);
   dispatch({
     type: ADD_TO_CART,
@@ -75,25 +64,39 @@ export const addItemsToCart = (id, quantity,cartItems) => async (dispatch) => {
     },
   });
 
-  const updatedCartItems = [...cartItems, { 
-    price: data.product.price,
-    product: data.product._id,
-    name: data.product.name,
-    image: data.product.images[0].url,
-    stock: data.product.stock,
-    quantity,
-  }];
-console.log(updatedCartItems)
-if (localStorage.getItem("cartItems")) {
-  console.log("presence")
+  let updatedCartItems;
+
+  if (localStorage.getItem("cartItems")) {
+    const cartItemsFromStorage = JSON.parse(localStorage.getItem("cartItems"));
+
+    const existingItemIndex = cartItemsFromStorage.findIndex(item => item.product === data.product._id);
+
+    if (existingItemIndex !== -1) {
+      // If the item already exists in cartItems, update its quantity
+      cartItemsFromStorage[existingItemIndex].quantity = quantity;
+      updatedCartItems = cartItemsFromStorage;
+    } else {
+      // If the item doesn't exist in cartItems, add it to the array
+      updatedCartItems = [...cartItemsFromStorage, {
+        price: data.product.price,
+        product: data.product._id,
+        name: data.product.name,
+        image: data.product.images[0].url,
+        stock: data.product.stock,
+        quantity,
+      }];
+    }
+  } else {
+    // If cartItems doesn't exist in localStorage, create it with the new item
+    updatedCartItems = [{
+      price: data.product.price,
+      product: data.product._id,
+      name: data.product.name,
+      image: data.product.images[0].url,
+      stock: data.product.stock,
+      quantity,
+    }];
+  }
+
   localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-}else{
-  console.log("not presence")
-  localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-}
 };
-
-
-
-
-
